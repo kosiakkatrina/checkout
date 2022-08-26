73ee82ad5b21cde3ca0e2c983586afa5d92f47fb
Kat <kosiak.katrina@gmail.com>
2022-08-26 17:01:54 +0100

    Refactor tests

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -2,30 +2,29 @@ require "checkout"
 
 describe Checkout do
     let(:checkout) { Checkout.new }
+    let(:fist_item) { "001" }
+    let(:second_item) { "002" }
+    let(:third_item) { "003" }
 
     it "has initial total of £0" do 
         expect(checkout.total).to eq(0)
     end
 
     it "allows the first item to be scanned" do 
-        item = "001"
-        checkout.scan(item)
+        checkout.scan(fist_item)
 
         expect(checkout.total).to eq(9.25)
     end
 
     it "allows the second item to be scanned" do 
-        item = "002"
-        checkout.scan(item)
-
+        checkout.scan(second_item)
+        
         expect(checkout.total).to eq(45.00)
     end
 
     it "allows schanning multiple items" do 
-        first_item = "002"
-        second_item = "003"
-        checkout.scan(first_item)
         checkout.scan(second_item)
+        checkout.scan(third_item)
 
         expect(checkout.total).to eq(64.95)
     end
