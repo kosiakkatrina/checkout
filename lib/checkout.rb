dbfec17e6333708621c98515033ccc630c8b3951
Kat <kosiak.katrina@gmail.com>
2022-08-26 17:43:11 +0100

    Apply provided discounts

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -3,11 +3,13 @@ class Checkout
 
     ITEMS = { "001" => { price: 9.25 }, "002" => { price: 45.00 }, "003" => { price: 19.95 } }
 
-    def initialize
+    def initialize(discounts = [])
         @total = 0
+        @discounts = discounts
     end
 
     def scan(item)
         @total += ITEMS[item][:price]
+        @discounts.each { |discount| @total = discount.call(total) }
     end
 end
