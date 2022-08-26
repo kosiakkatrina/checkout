346a7a481dba1858df56bb96c096116ea9ecd482
Kat <kosiak.katrina@gmail.com>
2022-08-26 17:42:26 +0100

    Add a test for applying a discount

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -2,7 +2,7 @@ require "checkout"
 
 describe Checkout do
     let(:checkout) { Checkout.new }
-    let(:fist_item) { "001" }
+    let(:first_item) { "001" }
     let(:second_item) { "002" }
     let(:third_item) { "003" }
 
@@ -11,14 +11,14 @@ describe Checkout do
     end
 
     it "allows the first item to be scanned" do 
-        checkout.scan(fist_item)
+        checkout.scan(first_item)
 
-        expect(checkout.total).to eq(9.25)
+        expect(checkout.total).to eq(9.25) 
     end
 
     it "allows the second item to be scanned" do 
         checkout.scan(second_item)
-        
+
         expect(checkout.total).to eq(45.00)
     end
 
@@ -27,5 +27,20 @@ describe Checkout do
         checkout.scan(third_item)
 
         expect(checkout.total).to eq(64.95)
+    end
+
+    context "when applying discounts" do
+        def over_60_discount(total)
+            total >= 60 ? total*0.9 : total
+        end
+
+        it "applies a provided discount" do
+            discounted_checkout = Checkout.new([method(:over_60_discount)])
+            discounted_checkout.scan(first_item)
+            discounted_checkout.scan(second_item)
+            discounted_checkout.scan(third_item)
+
+            expect(discounted_checkout.total).to be(66.78)
+        end
     end
 end
