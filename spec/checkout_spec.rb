6865282493caa813efa3a48fbedf8a72fe1c87b1
Kat <kosiak.katrina@gmail.com>
2022-08-26 16:27:34 +0100

    Add a test for scan item

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -5,4 +5,12 @@ describe Checkout do
         co = Checkout.new
         expect(co.total).to eq(0)
     end
+
+    it "allows items to be scanned" do 
+        co = Checkout.new
+        item = "001"
+        co.scan(item)
+        
+        expect(co.total).to eq(9.25)
+    end
 end
