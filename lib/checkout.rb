7bd39262c07d4669296fc5f0c533164cb1169ff4
Kat <kosiak.katrina@gmail.com>
2022-08-27 09:03:17 +0100

    Apply bulk discount

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,15 +1,17 @@
 class Checkout 
     attr_reader :total
 
-    ITEMS = { "001" => { price: 9.25 }, "002" => { price: 45.00 }, "003" => { price: 19.95 } }
+    ITEMS = { "001" => { price: 9.25, bulk_discount_amount: 0.75 }, "002" => { price: 45.00 }, "003" => { price: 19.95 } }
 
     def initialize(discounts = [])
         @total = 0
+        @scanned_items = []
         @discounts = discounts
     end
 
     def scan(item)
         @total += ITEMS[item][:price]
-        @discounts.each { |discount| @total = discount.call(total) }
+        @scanned_items << item
+        @discounts.each { |discount| @total = discount.call(@total, @scanned_items) }
     end
 end
