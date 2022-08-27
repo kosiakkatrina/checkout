9d0a5e17c0a9a1bf29f8b531204149f7e448c10e
Kat <kosiak.katrina@gmail.com>
2022-08-27 09:38:46 +0100

    Refactor tests

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -7,27 +7,28 @@ describe Checkout do
     let(:second_item) { "002" }
     let(:third_item) { "003" }
 
-    it "has initial total of £0" do 
-        expect(checkout.total).to eq(0)
-    end
+    context "with no discounts" do
+        it "has initial total of £0" do 
+            expect(checkout.total).to eq(0)
+        end
 
-    it "allows the first item to be scanned" do 
-        checkout.scan(first_item)
+        it "allows the first item to be scanned" do 
+            checkout.scan(first_item)
 
-        expect(checkout.total).to eq(9.25) 
-    end
+            expect(checkout.total).to eq(9.25) 
+        end
 
-    it "allows the second item to be scanned" do 
-        checkout.scan(second_item)
+        it "allows the second item to be scanned" do 
+            checkout.scan(second_item)
 
-        expect(checkout.total).to eq(45.00)
-    end
+            expect(checkout.total).to eq(45.00)
+        end
 
-    it "allows schanning multiple items" do 
-        checkout.scan(second_item)
-        checkout.scan(third_item)
+        it "allows scanning multiple items" do 
+            [second_item, third_item].each { |item| checkout.scan(item)}
 
-        expect(checkout.total).to eq(64.95)
+            expect(checkout.total).to eq(64.95)
+        end
     end
 
     context "when applying discounts" do
@@ -41,28 +42,21 @@ describe Checkout do
 
         it "applies a provided total discount" do
             discounted_checkout = Checkout.new([method(:over_60_discount)])
-            discounted_checkout.scan(first_item)
-            discounted_checkout.scan(second_item)
-            discounted_checkout.scan(third_item)
+            [first_item, second_item, third_item].each { |item| discounted_checkout.scan(item)}
 
             expect(discounted_checkout.total).to eq(66.78)
         end
 
         it "applies a provided item discount" do
             discounted_checkout = Checkout.new([method(:very_cheap_chairs_discount)])
-            discounted_checkout.scan(first_item)
-            discounted_checkout.scan(third_item)
-            discounted_checkout.scan(first_item)
+            [first_item, third_item, first_item].each { |item| discounted_checkout.scan(item)}
 
             expect(discounted_checkout.total).to eq(36.95)
         end
 
         it "applies multiple provided discounts" do
             discounted_checkout = Checkout.new([method(:very_cheap_chairs_discount), method(:over_60_discount)])
-            discounted_checkout.scan(first_item)
-            discounted_checkout.scan(second_item)
-            discounted_checkout.scan(first_item)
-            discounted_checkout.scan(third_item)
+            [first_item, second_item, first_item, third_item].each { |item| discounted_checkout.scan(item)}
 
             expect(discounted_checkout.total).to eq(73.76)
         end
